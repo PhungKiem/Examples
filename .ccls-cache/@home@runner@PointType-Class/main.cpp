3fe6edc736bd3375aa7d71c5a87f0ca6522ff62b
PhungKiem <119937647+PhungKiem@users.noreply.github.com>
2024-09-08 03:51:35 +0000

    PointType-class

--- .ccls-cache/@home@runner@PointType-Class/main.cpp
+++ .ccls-cache/@home@runner@PointType-Class/main.cpp
@@ -1,1 +1,34 @@
-//This File is for
+//This File is The main to test the functions of the PointType Class
+
+#include <iostream>
+#include <iomanip>
+#include "PointType.h"
+using namespace std;
+
+int main()
+{
+
+  cout<< "From Default Constructor: " << endl;
+  PointType<int> p1I;
+  PointType<double> p1D;
+    cout << "Integer Point:";
+    p1I.print();
+    cout << "Double Point:";
+    p1D.print();
+
+  cout << "\nConstrutor with Parameters: " << endl;
+  PointType<int> p2I(3,5);
+  PointType<double> p2D(3.5,5.5);
+  cout << "Integer Point:";
+  p2I.print();
+  cout << "Double Point:";
+  p2D.print();
+
+  cout << "\n(integer) Distance =";
+  double distance = p1I - p2I;
+  cout << distance << endl;
+
+  cout << "\n(double) Distance =";
+  distance = p1D - p2D;
+  cout << distance << endl;
+}
